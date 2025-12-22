38201a1c178e680f73184e346c8eb42bda26a560
George McNamara <68518082+GeoGeo116@users.noreply.github.com>
2025-12-22 19:22:21 +0000

    Added functional side navigation

---
+++ scripts.js
@@ -0,0 +1,9 @@
+function openNav(){
+    document.getElementById("sidenav").style.display = "flex";
+    document.body.style.backgroundColor = "rgba(0, 0, 0, 0.93)";
+}
+
+function closeNav(){
+    document.getElementById("sidenav").style.display = "none";
+    document.body.style.backgroundColor = "#0F172A";
+}
