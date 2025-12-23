e28b62ba26e0cbfba8053051319c143119d6ad8a
George McNamara <68518082+GeoGeo116@users.noreply.github.com>
2025-12-23 21:21:35 +0000

    Added animation to sidenav mobile view

--- scripts.js
+++ scripts.js
@@ -1,9 +1,17 @@
 function openNav(){
-    document.getElementById("sidenav").style.display = "flex";
+    document.getElementById("sidenav").style.left = "0px";
     document.body.style.backgroundColor = "rgba(0, 0, 0, 0.93)";
 }
 
 function closeNav(){
-    document.getElementById("sidenav").style.display = "none";
+    document.getElementById("sidenav").style.left = "-180px";
     document.body.style.backgroundColor = "#0F172A";
+}
+
+function toAbout(){
+    closeNav();
+}
+
+function toProjects(){
+    closeNav();
 }
