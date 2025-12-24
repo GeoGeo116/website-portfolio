480f5613e2e2bc37c2d4a0be8c7ee34d34055123
George McNamara <68518082+GeoGeo116@users.noreply.github.com>
2025-12-24 16:38:31 +0000

    Fixed layout issue on mobile, hid sidenav glow while closed

--- scripts.js
+++ scripts.js
@@ -1,11 +1,13 @@
 function openNav(){
     document.getElementById("sidenav").style.left = "0px";
     document.body.style.backgroundColor = "rgba(0, 0, 0, 0.93)";
+    document.getElementById("sidenav").style.boxShadow = "0px 0px 10px 0px #2DD4BF";
 }
 
 function closeNav(){
     document.getElementById("sidenav").style.left = "-180px";
     document.body.style.backgroundColor = "#0F172A";
+    document.getElementById("sidenav").style.boxShadow = "none";
 }
 
 function toSection(Section){
